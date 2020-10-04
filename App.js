f5acb525f174152f4b7eb13a604ee3f79e72ec18
bekzodnj <bekzodnx@gmail.com>
2020-10-04 18:30:04 +0100

    Added UI with ScrollView, TextInput and Buttons

--- App.js
+++ App.js
@@ -1,26 +1,62 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState(
-    'Starter text. Click the button to change it',
-  );
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const onChangeHandler = (newGoalText) => {
+    setEnteredGoal(newGoalText);
+  };
+
+  const submitHandler = () => {
+    enteredGoal &&
+      setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+
+    console.log(courseGoals);
+  };
+
+  const clearHandler = () => {
+    setCourseGoals([]);
+
+    console.log(courseGoals);
+  };
 
   return (
-    <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter you goal here..." style={styles.input} />
-        <Button title="ADD" />
-      </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Enter you goal here..."
+            style={styles.input}
+            onChangeText={onChangeHandler}
+          />
+          <Button title="ADD" onPress={submitHandler} />
+          <Button title="Clear" onPress={clearHandler} />
+        </View>
 
-      <View></View>
-    </View>
+        <ScrollView>
+          {courseGoals.map((goal, idx) => (
+            <View key={idx} style={styles.goalItem}>
+              <Text>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 30,
+    padding: 20,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -32,6 +68,13 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     padding: 10,
     margin: 10,
-    width: '80%',
+    width: '60%',
+  },
+  goalItem: {
+    padding: 10,
+    borderColor: 'black',
+    backgroundColor: '#ccc',
+    borderWidth: 1,
+    marginVertical: 10,
   },
 });
